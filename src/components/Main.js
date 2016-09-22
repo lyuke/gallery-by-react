4e5d680c15cb961d59dc2cf9a11dc86e061db0ad
Lyuke <lk11nju@gmail.com>
2016-09-22 18:57:20 +0800

    add images

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,4 @@
-require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
@@ -15,15 +14,42 @@ function genImageURL(imageDatasArr) {
 
 imageDatas = genImageURL(imageDatas);
 
+class ImgFigure extends React.Component {
+
+	render() {
+		return (
+			<figure className="img-figure">
+				<img src={this.props.data.imageURL}
+				alt={this.props.data.title} />
+				<figcaption>
+					<h2 className="img-title">{this.props.data.title}</h2>
+				</figcaption>
+			</figure>
+
+		);
+	}
+
+}
+
+
+
 class AppComponent extends React.Component {
 	render() {
+		let controllerUnits = [],
+			imgFigures = [];
+
+		imageDatas.forEach(function(img) {
+			imgFigures.push(<ImgFigure data={img} key={img.fileName}/>);
+		})
+
+
 		return (
 			<section className="stage">
 				<section className="img-sec">
-					
+					{imgFigures}
 				</section>
 				<nav className = "controller-nav" >
-					
+					{controllerUnits}
 				</nav>
 			</section>
 		);
